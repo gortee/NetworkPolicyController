4dbe78c73b3e045f30e501632552e3ed4db24deb
Aaron Torgerson <torgerson.aaron@gmail.com>
2019-06-04 13:43:11 -0700

    initial add of logic

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+rm -rf artifacts/*
+
+export CGO_ENABLED=0
+export GOARCH="${ARCH}"
+export GOOS="${OS}"
+
+# build linux
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o autonetpol main.go
+#tar -czf artifacts/nsxt-trace-linux.tgz artifacts/nsxt-trace
